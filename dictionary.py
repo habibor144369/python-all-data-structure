f41692b21d54ff647bf918d3953b33aa789bdb91
Your Name <you@example.com>
2020-12-09 01:14:55 -0800

    hi programmer

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,13 @@
-# dictionary in dictionary, list tuple,---
-items = {'name': 'habib', 'id': 14, 'phone': '01768280237', 'numbers': [23, 45, 67, 78, 97]}
-print(items['numbers'], type(items['numbers']))
+# dictionary data structure---
+while True:
+    marks = [67, 56, 89, 96, 47, 97, 75, 87, 86, 80]
+    roll = input('please enter your roll number : ')
+    print('your marks is', marks[int(roll) -1])
+
+# dictionary data structure--- same things--
+while True:
+    marks = [67, 56, 89, 96, 47, 97, 75, 87, 86, 80]
+    roll = int(input('please enter your roll number : '))
+    print('your marks is', marks[(roll) -1])
+
+
